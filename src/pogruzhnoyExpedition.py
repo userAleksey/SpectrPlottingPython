5a66bf1767b9d1a8a8a5e8ab91dc735e846c916b
userAleksey <40539422+userAleksey@users.noreply.github.com>
2018-07-30 16:17:17 +1000

    pogrEdit

--- src/pogruzhnoyExpedition.py
+++ src/pogruzhnoyExpedition.py
@@ -15,7 +15,7 @@ import datetime
 #style.use('ggplot')
 
 do_save = 0
-rad_type = '527'
+rad_type = '527 nm'
 tos = 'mwater'
 day_of_exp = 'pogruzhnoyExp2018'
 
@@ -40,12 +40,20 @@ if tos == 'mwater':
     fData1 = {}
     wl = np.loadtxt(join(dir_f1,listdir(dir_f1)[0]),usecols = 0, comments='>')
     for itm in listdir(dir_f1):
+        if itm.endswith("zond.txt"):
+            zondVals = np.loadtxt(join(dir_f1,itm), skiprows=1)
+            continue
+        if itm.endswith("callib_coefs_part.txt"):
+            calibVals = np.loadtxt(join(dir_f1,itm),usecols = 0, comments='>')
+            continue
         if itm.endswith(".txt"):
             fData1[itm] = np.loadtxt(join(dir_f1,itm),usecols = 1, comments='>')
     
 ncfData1 = {}
-for itm in listdir(fData1[:-2]):
-    ncfData1[itm] = fData1[itm]*fData1[:-1]
+for itm in fData1:
+    ncfData1[itm] = fData1[itm]*calibVals
+    ncfData1[itm] = ncfData1[itm]/max(ncfData1[itm][:100])
+    
 
 
         
@@ -56,7 +64,7 @@ if not isdir(res_dir):
     makedirs(abspath(res_dir))
             
         
-fData_plot = fData1
+fData_plot = ncfData1
 
 
 fig, (ax1) = plt.subplots(1,1,figsize=(18,10))
@@ -65,16 +73,38 @@ fig.set_tight_layout(True)
 for itm in fData_plot:
     ax1.plot(wl,fData_plot[itm])
 
-ax1.legend(np.hstack([fData_plot.keys]))
+ax1.legend(list(fData_plot.keys()))
 
 ax1.set(xlabel = 'Wavelength, nm', 
        ylabel = 'I, Relative units', 
        title = rad_type+' '+tos,
-       xlim = [340,800],
-       ylim = [0,]
+       xlim = [565,790],
+       ylim = [0,1.2]
        )
 if do_save == 1:
-    fig.savefig(sna+'340-800.jpg',transparent=False,dpi=300,bbox_inches="tight")       
+    fig.savefig(sna+'.jpg',transparent=False,dpi=300,bbox_inches="tight")       
+
+plt.show()
+
+fData_plot = zondVals
+
+
+fig, (ax1) = plt.subplots(1,1,figsize=(18,10))
+fig.set_tight_layout(True)
+
+for itm in fData_plot[0][1:]:
+    ax1.plot(fData_plot[:][0],fData_plot[itm])
+
+ax1.legend(list(fData_plot.keys()))
+
+ax1.set(xlabel = 'Wavelength, nm', 
+       ylabel = 'I, Relative units', 
+       title = rad_type+' '+tos,
+       xlim = [565,790],
+       ylim = [0,1.2]
+       )
+if do_save == 1:
+    fig.savefig(sna+'.jpg',transparent=False,dpi=300,bbox_inches="tight")       
 
 plt.show()
     
