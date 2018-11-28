3948a38193dbd320d5a2bbc4c37d92cfb63c42e2
userAleksey <40539422+userAleksey@users.noreply.github.com>
2018-11-28 15:50:13 +1000

    Add files via upload

--- src/flu_of_oil.py
+++ src/flu_of_oil.py
@@ -21,10 +21,13 @@ def natural_sort(l):
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
 
-do_save = 1
+do_save = 0
 rad_type = ''
-day_of_exp = '15_10_2018'
-tos = 'undentif_led'
+day_of_exp = '28_11_2018'
+tos = 'led'
+
+norm_max = 1
+smooth = 0
 
 font = {'family' : 'normal',
         'weight' : 'bold',
@@ -45,7 +48,6 @@ dir = join('..','data',day_of_exp,tos)
 dir_d1 = dir
 Data1 = {}
 #wl = np.loadtxt(join(dir_d1,listdir(dir_d1)[0]),skiprows = 14, usecols = 0, comments='>')
-
 wl= np.loadtxt(join(dir_d1,listdir(dir_d1)[0]), dtype=np.str, usecols = 0, skiprows=14)
 wl = np.char.replace(wl, ',', '.').astype(np.float64)
         #wl = np.char.replace(wl, '\'', '')
@@ -66,6 +68,17 @@ if not isdir(res_dir):
 fig, (ax1) = plt.subplots(1,1,figsize=(18,10))
 fig.set_tight_layout(True)
 
+if norm_max == 1:
+    for itm in Data1:
+        Data1[itm] = Data1[itm]/max(Data1[itm])
+
+
+if smooth == 1:
+    for itm in Data1:
+        Data1[itm] = savitzky_golay(Data1[itm], 51, 3)
+
+
+
 for itm in Data1:
     ax1.plot(wl,Data1[itm])
 
@@ -74,7 +87,7 @@ ax1.legend(list(Data1.keys()),loc = 0)
 ax1.set(xlabel = 'Wavelength, nm', 
        ylabel = 'I, units', 
        title = rad_type+' '+tos,
-       xlim = [250,600],
+       xlim = [250,1000],
  #      ylim = [0,]
        )    
        
@@ -82,33 +95,3 @@ if do_save == 1:
     fig.savefig(sna+'.jpg',transparent=False,dpi=300,bbox_inches="tight")       
 
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
