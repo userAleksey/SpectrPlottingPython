6ec6cc1ea26194e699e85d13b713d82f82ee22a9
userAleksey <40539422+userAleksey@users.noreply.github.com>
2020-07-29 16:43:35 +1000

    Add files via upload

--- src/somedataproc/managedata.py
+++ src/somedataproc/managedata.py
@@ -23,6 +23,21 @@ def load_data (datapath):
 
     return Data
 
+def load_data_with_sel (datapath, y_val_for_sel):
+    Data = {}
+    #for itm in natural_sort(listdir(datapath)):
+    for itm in listdir(datapath):
+        if itm.endswith(".txt"):
+            data_itm = np.loadtxt(join(datapath, itm), dtype=np.str, skiprows=17, usecols=1, comments='>')
+            data_itm = np.char.replace(data_itm, ',', '.').astype(np.float64)
+            if max(data_itm) > y_val_for_sel:
+                Data[itm] = data_itm
+        if itm.endswith(".tif"):
+            Data[itm] = np.sum(np.array(Image.open(join(datapath, itm)), dtype='float64')/65536.,0)
+            Data[itm] = Data[itm][::-1]
+
+    return Data
+
 def average (data):
     dictlist = []
     Data = {}
